f75a883a6913f14d8736fb16b17f0e0400e9396c
taehwanNoh <thnoh93@naver.com>
2020-07-06 16:44:24 +0900

    기본 환경 설정

--- src/main/java/com/spring/thnoh/helloSpringBoot/HelloSpringBootApplication.java
+++ src/main/java/com/spring/thnoh/helloSpringBoot/HelloSpringBootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringBootApplication.class, args);
     }
--- src/main/java/com/spring/thnoh/helloSpringBoot/controller/HomeController.java
+++ src/main/java/com/spring/thnoh/helloSpringBoot/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.spring.thnoh.helloSpringBoot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("/")
     public String home(Model model){
+
+        logger.debug("Calling home( )");
 
         model.addAttribute("message","hello world");
         return "index";
